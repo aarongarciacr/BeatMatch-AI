0dd30462097dd962431bf204c2b628345002cda2
aarongarciacr <aarongarcia4997@gmail.com>
2025-03-13 15:04:34 -0700

    Add icon to dasboard generate button

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -501,7 +501,30 @@ const Dashboard = () => {
               Generating...
             </span>
           ) : (
-            "Generate Playlist"
+            <div className="flex items-center justify-center gap-2 font-bold">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="26"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="1.3333333333333333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-wand-sparkles"
+              >
+                <path d="m21.64 3.64-1.28-1.28a1.21 1.21 0 0 0-1.72 0L2.36 18.64a1.21 1.21 0 0 0 0 1.72l1.28 1.28a1.2 1.2 0 0 0 1.72 0L21.64 5.36a1.2 1.2 0 0 0 0-1.72" />
+                <path d="m14 7 3 3" />
+                <path d="M5 6v4" />
+                <path d="M19 14v4" />
+                <path d="M10 2v2" />
+                <path d="M7 8H3" />
+                <path d="M21 16h-4" />
+                <path d="M11 3H9" />
+              </svg>
+              Generate Playlist
+            </div>
           )}
         </button>
       </div>
