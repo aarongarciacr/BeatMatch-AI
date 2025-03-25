08dacd62efb874fe07a7674de5a192324ba8c6bc
aarongarciacr <aarongarcia4997@gmail.com>
2025-03-24 20:23:27 -0700

    Fix svg import on login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,8 @@
 import WhiteLogo from "../assets/Primary_Logo_White_CMYK.svg";
 import FeatureCard from "../component/FeatureCard";
 import Mood from "../assets/Mood.svg";
-import Sprint from "../assets/sprint.svg";
-import Tune from "../assets/tune.svg";
+import Sprint from "../assets/Sprint.svg";
+import Tune from "../assets/Tune.svg";
 import BG from "../assets/bg-img.jpg";
 import Footer1 from "../component/Footer/Footer1";
 
