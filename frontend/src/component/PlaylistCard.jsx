f99b76df6dbca2a3c6075b97d10bae4f622f2c68
aarongarciacr <aarongarcia4997@gmail.com>
2025-03-27 15:51:09 -0700

    Add condition to check playlist.id or playlist.SpotifyID on playlistCard

--- frontend/src/component/PlaylistCard.jsx
+++ frontend/src/component/PlaylistCard.jsx
@@ -13,8 +13,8 @@ const PlaylistCard = ({ playlist }) => {
   const dispatch = useDispatch();
 
   const handlePlaylistClick = () => {
-    if (playlist.href) {
-      navigate(`/playlists/${playlist.id}`);
+    if (playlist.id || playlist.spotifyId) {
+      navigate(`/playlists/${playlist.id || playlist.spotifyId}`);
     } else {
       navigate(`/playlists/BM/${playlist._id}`);
     }
